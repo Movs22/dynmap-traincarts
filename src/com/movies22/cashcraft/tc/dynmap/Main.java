147fa8d380e3289774c8b5500905a8725cb93f80
Movies22 <111647781+Movs22@users.noreply.github.com>
2023-10-16 20:54:26 +0100

    Testing
    .

---
+++ src/com/movies22/cashcraft/tc/dynmap/Main.java
@@ -0,0 +1,17 @@
+package com.movies22.cashcraft.tc.dynmap;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	public TrainCarts traincarts;
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
