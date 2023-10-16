705dfc52da36efad40430af6536e7c9643506e44
Movies22 <111647781+Movs22@users.noreply.github.com>
2023-10-16 21:01:46 +0100

    Update Main.java

--- src/com/movies22/cashcraft/tc/dynmap/Main.java
+++ src/com/movies22/cashcraft/tc/dynmap/Main.java
@@ -1,13 +1,22 @@
 package com.movies22.cashcraft.tc.dynmap;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.dynmap.DynmapAPI;
+
+import com.movies22.cashcraft.tc.TrainCarts;
+
+
 
 public class Main extends JavaPlugin {
 
 	public TrainCarts traincarts;
+	
+	public DynmapAPI dynmapAPI;
+	
 	@Override
 	public void onEnable() {
-		
+		dynmapAPI = (DynmapAPI) getServer().getPluginManager().getPlugin("dynmap");
+		traincarts = (TrainCarts) getServer().getPluginManager().getPlugin("TrainCarts2"); 
 	}
 	
 	@Override
