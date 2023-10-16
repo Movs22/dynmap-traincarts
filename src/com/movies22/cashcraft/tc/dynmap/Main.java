aa4e874fce83a60305bf3b5cd698132c819f2177
Movies22 <111647781+Movs22@users.noreply.github.com>
2023-10-16 21:18:16 +0100

    classpath update + tc2 test

--- src/com/movies22/cashcraft/tc/dynmap/Main.java
+++ src/com/movies22/cashcraft/tc/dynmap/Main.java
@@ -1,9 +1,12 @@
 package com.movies22.cashcraft.tc.dynmap;
 
+import java.util.logging.Level;
+
 import org.bukkit.plugin.java.JavaPlugin;
 import org.dynmap.DynmapAPI;
 
 import com.movies22.cashcraft.tc.TrainCarts;
+import com.movies22.cashcraft.tc.api.MetroLines.MetroLine;
 
 
 
@@ -16,7 +19,9 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		dynmapAPI = (DynmapAPI) getServer().getPluginManager().getPlugin("dynmap");
-		traincarts = (TrainCarts) getServer().getPluginManager().getPlugin("TrainCarts2"); 
+		traincarts = (TrainCarts) getServer().getPluginManager().getPlugin("TrainCarts2");
+		MetroLine l = traincarts.lines.createLine("TEST", "#ff0000");
+		getServer().getLogger().log(Level.INFO, "Created " + l);
 	}
 	
 	@Override
